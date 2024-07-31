6ec7939f72a91c30a2373850262fdeb0f84887e2
namague00 <alfaiztaufiqurrahman3@gmail.com>
2024-07-31 10:24:52 +0700

    tugas kelar

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void cetakdata(string nama,string nisn,string jurusan,int tinggibadan){
+    cout << "Nama : " << nama << endl;
+    cout << "Nisn : " << nisn << endl;
+    cout << "Jurusan : " << jurusan <<endl;
+    cout << "Tinggi Badan : "<< tinggibadan;
+}
+
+
 int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
+    string nama,nisn,jurusan;
+    int tinggibadan;
+    cout<<"masukkan nama: ";cin >> nama;
+    cout <<"masukkan nisn: ";cin >> nisn;
+    cout<<"masukkan jurusan: ";cin >> jurusan;
+    cout <<"masukkan tinggi badan: ";cin >> tinggibadan;
+
+    cetakdata(nama,nisn,jurusan,tinggibadan);
     return 0;
 }
